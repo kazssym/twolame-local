917674cfb157326631a580f0e7c92dec250265d9
Kaz Sasa <kazssym@vx68k.org>
2013-03-17 18:46:51 +0900

    Created a file to specify the library to link in stwolame.

---
+++ simplefrontend/_libs.c
@@ -0,0 +1,10 @@
+/*
+ * Library specification(s) for C++Builder.
+ * Written in 2013 by Kaz Sasa <https://bitbucket.org/kazssym>
+ *
+ * To the extent possible under law, the author(s) have dedicated all copyright
+ * and related and neighboring rights to this file to the public domain
+ * worldwide.  This file is distributed without any warranty.
+ */
+
+#pragma comment(lib, "libtwolame")
